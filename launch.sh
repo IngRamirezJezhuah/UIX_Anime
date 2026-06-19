dcce0b146c71e66c10189904f593e2969363324a
IngRamirezJezhuah <ramierzalvaradodavidjezhuah@gmail.com>
2026-06-18 18:58:15 -0600

    funcionalidad + rutas acomodadas

--- launch.sh
+++ launch.sh
@@ -3,6 +3,9 @@
 eww kill 
 
 DIR="$(dirname "$0")"
+CFG_DIR="$DIR/Front"
 
-eww -c "$DIR/Front" open main_window
+eww --config "$CFG_DIR" daemon &
+sleep 0.1
+eww -c "$CFG_DIR" open --toggle main_window
 
